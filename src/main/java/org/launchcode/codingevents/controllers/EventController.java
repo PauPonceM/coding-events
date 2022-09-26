b4a713a93c34dd910f1988be5077cfb62afff33b
Paulina Ponce <paulina.poncemarcano@gmail.com>
2022-09-26 13:24:49 -0400

    finished studio 17

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,57 +1,70 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    @Autowired
+    private EventRepository eventRepository;
 
     @GetMapping
-    public String displayAllEvents(Model model){
-       model.addAttribute("events", EventData.getAll());
-       return "events/index";
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", eventRepository.findAll());
+        return "events/index";
     }
 
-
-    @GetMapping("create") //lives at /events/create
-    public String renderCreateEventForm(Model model){
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
         model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
-        if(errors.hasErrors()){
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
             model.addAttribute("title", "Create Event");
             return "events/create";
         }
-    EventData.add(newEvent);
-    return "redirect:"; //instructs the browser to go to the root path of the controller= events
+
+        eventRepository.save(newEvent);
+        return "redirect:";
     }
 
     @GetMapping("delete")
-    public String displayDeleteEventForm(Model model){
+    public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
     @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam int[] eventIds){
-    for(int id:eventIds){
-        EventData.remove(id);
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                eventRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:";
     }
-    return "redirect:";
-    }
+
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,42 +1,48 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-public class Event {
-    private int id;
-    private static int nextId=1;
+/**
+ * Created by Chris Bay
+ */
+@Entity
+public class Event extends AbstractEntity{
+
 
     @NotBlank(message = "Name is required")
-    @Size(min=3, max=50, message = "Name must be between 3 and 50 characters long")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-    @Size(max=500, message = "Description too long")
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
+    private EventType type;
 
-    public Event(String name, String description, String contactEmail) {
+    public Event(String name, String description, String contactEmail, EventType type) {
         this.name = name;
-        this.description= description;
+        this.description = description;
         this.contactEmail = contactEmail;
-        this.id= nextId;
-        nextId++;
+        this.type = type;
+    }
+    public Event() {}
+    public EventType getType() {
+        return type;
     }
 
-    public Event(){}
-
-    public String getContactEmail() {
-        return contactEmail;
+    public void setType(EventType type) {
+        this.type = type;
     }
 
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
 
     public String getName() {
         return name;
@@ -54,25 +60,21 @@ public class Event {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
+    public String getContactEmail() {
+        return contactEmail;
     }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+
+
+
 
     @Override
     public String toString() {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
